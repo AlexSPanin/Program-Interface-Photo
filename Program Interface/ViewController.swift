0e6eff1fa96708c3b8b41cb5d080d45fa636fbbd
Alexander Panin <panin1970@gmail.com>
2022-02-04 15:43:54 +0300

    Initial Commit

---
+++ Program Interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Program Interface
+//
+//  Created by Александр Панин on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
