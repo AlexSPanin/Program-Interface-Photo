a81a63891c3822bc875a9908cac3dc832f974a90
Alexander Panin <panin1970@gmail.com>
2022-02-06 10:16:50 +0300

    add rotated

---
+++ Program Interface/ImageScrollView.swift
@@ -0,0 +1,194 @@
+//
+//  ImageScrollView.swift
+//  Program Interface
+//
+//  Created by Александр Панин on 04.02.2022.
+//
+
+// клас для прокрутки и перемещения изображения!!!
+
+import UIKit
+
+class ImageScrollView: UIScrollView, UIScrollViewDelegate {
+    
+    var imageZoomView: UIImageView?
+    
+    // MARK: - переменная для авто зуминга при двойном нажатии
+    //Дискретный распознаватель жестов, который интерпретирует одно или несколько касаний.
+    lazy var zoomingTap: UITapGestureRecognizer = {
+        let zoomingTap = UITapGestureRecognizer(target: self, action: #selector(handleZoomingTap))
+        zoomingTap.numberOfTapsRequired = 2
+        return zoomingTap
+    }()
+    
+    // MARK: - передаем что наш класс должен использовать своства делегата
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // сообщаем кто будет выполнять функции делегата
+        delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - обновление интерфейса
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        positionImage()
+    }
+    
+    // MARK: - загрузка изображения в ScrollView
+    func set(image: UIImage) {
+        // удаляет предыдущую фотографию при переиспользовании ячейки
+        imageZoomView?.removeFromSuperview()
+        imageZoomView = nil
+        
+        // инициализируем объект нашим изображением
+        imageZoomView = UIImageView(image: image)
+        
+        // после проверки опционала добавляем наше subview
+        guard let imageZoomView = imageZoomView else { return }
+        
+        addSubview(imageZoomView)
+        
+        // задаем входные размеры изображения
+        configurate(imageSize: image.size)
+    }
+    // MARK: - Обрезка изображения
+    func getImage(image: UIImage) -> UIImage {
+        
+        let width: CGFloat = bounds.width / zoomScale
+        let height: CGFloat = bounds.height / zoomScale
+        let x = bounds.origin.x / zoomScale
+        let y = bounds.origin.y / zoomScale
+        
+        let cgRect = CGRect(x: x,
+                            y: y,
+                            width: width,
+                            height: height)
+        
+        let orientation = image.imageOrientation
+        let scale = image.scale
+        let cgImage = image.cgImage
+
+        if let cropperCGImage = cgImage?.cropping(to: cgRect) {
+            let context = CIContext(options: nil)
+            let ciImage = CIImage(cgImage: cropperCGImage)
+            if let refImage = context.createCGImage(ciImage, from: ciImage.extent) {
+                let uiImage = UIImage(cgImage: refImage, scale: scale, orientation: orientation)
+                return uiImage
+            }
+        }
+        return UIImage()
+    }
+    
+    // MARK: - задаем методы делегата UIScrollViewDelegate
+    // Запрашивает делегата о масштабировании вида при приближении масштабирования на виде прокрутки.
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageZoomView
+    }
+    // Сообщает делегату, что коэффициент масштабирования вида прокрутки изменился.
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        positionImage()
+    }
+    
+    // функция отрпбатывающая нажание на экран см в переменной
+    @objc func handleZoomingTap(sender: UITapGestureRecognizer) {
+        // передаем где нажатие
+        let location = sender.location(in: sender.view)
+        zoomToTap(point: location, animated: true)
+    }
+    // прописываем логику при первом нажатии 2 раза - увеличивается, а при повторном уменьшается
+    private func zoomToTap(point: CGPoint, animated: Bool) {
+        let currectScale = zoomScale
+        let minScale = minimumZoomScale
+        let maxScale = maximumZoomScale
+        
+        if (minScale == maxScale && minScale > 1) {
+            return
+        }
+        let toScale = maxScale
+        let finalScale = (currectScale == minScale ) ? toScale : minScale
+        
+        let zoomRect = zoomRect(scale: finalScale, center: point)
+        // встроенная ункция
+        zoom(to: zoomRect, animated: animated)
+    }
+    private func zoomRect(scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        let bounds = bounds
+        
+        zoomRect.size.width = bounds.size.width / scale
+        zoomRect.size.height = bounds.size.height / scale
+        
+        zoomRect.origin.x = center.x - (zoomRect.size.width / 2)
+        zoomRect.origin.y = center.y - (zoomRect.size.height / 2)
+        
+        return zoomRect
+    }
+    
+    // MARK: - загрузка размеров изображения
+    private func configurate(imageSize: CGSize) {
+        
+        // передаем размеры картинки
+        contentSize = imageSize
+        // задаем параметры зуминга
+        setZoomScale()
+        
+        // свойству zoomScale у UIScrollView присваиваем минимальный зум
+        zoomScale = minimumZoomScale
+        
+        imageZoomView?.addGestureRecognizer(zoomingTap)
+        imageZoomView?.isUserInteractionEnabled = true
+        
+        // удаляем горизонтальные и вертикальные прокрутки
+        showsVerticalScrollIndicator = false
+        showsHorizontalScrollIndicator = false
+        
+        // скорость зуминга
+        decelerationRate = UIScrollView.DecelerationRate.normal
+    }
+    // MARK: - установка параметров изображения после зуминга
+    private func setZoomScale() {
+        //  извлечение опционала
+        guard let imageZoomView = imageZoomView else { return }
+        // фиксируем размеры рамки и изображения
+        let boundsSize = bounds.size
+        let imageSize = imageZoomView.bounds.size
+        // вычисляем соотношения экранов по x и y
+        let xScale = boundsSize.width / imageSize.width
+        let yScale = boundsSize.height / imageSize.height
+        // определяем минимальный и максимальный зум
+        let minScale = min(xScale, yScale)
+        let maxScale: CGFloat = max(0.5, minScale)
+        // задаем параметры минимального и максимального зума
+        minimumZoomScale = minScale
+        maximumZoomScale = maxScale
+    }
+    
+    // MARK: - задаем место расположения картинки после зума
+    private func positionImage() {
+        //  извлечение опционала
+        guard let imageZoomView = imageZoomView else { return }
+        // фиксируем размеры рамки и изображения
+        let boundsSize = bounds.size
+        
+        var frameToCenter = imageZoomView.frame
+        // если размеры высоты view при зум меньше ширины экрана то view вписывается в ширику экрана
+        if frameToCenter.size.height < boundsSize.height {
+            frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2
+        } else {
+            frameToCenter.origin.y = 0
+        }
+        // если размеры ширины view при зум меньше ширины экрана то view вписывается в ширику экрана
+        if frameToCenter.size.width < boundsSize.width {
+            frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2
+        } else {
+            frameToCenter.origin.x = 0
+        }
+       
+        // присваиваем новый frame
+        imageZoomView.frame = frameToCenter
+    }
+}
---
+++ Program Interface/MainViewController.swift
@@ -0,0 +1,278 @@
+//
+//  ViewController.swift
+//  Program Interface
+//
+//  Created by Александр Панин on 04.02.2022.
+//
+
+import UIKit
+
+// MARK: - type source
+enum TypeSource {
+    case camera
+    case gallary
+}
+
+class MainViewController: UIViewController {
+    
+    // создаем объект класса ImageScrollView для изменения размеров
+    
+    var imagePhotoPicker = UIImagePickerController()
+    var imageScroll = ImageScrollView()
+    var photoImage: UIImage = {
+        if let imagePath = Bundle.main.path(forResource: "DSC", ofType: "JPG")
+        {
+            if let photo = UIImage(contentsOfFile: imagePath) { return photo }
+        }
+        return UIImage()
+    }()
+    
+    // создали кнопки
+    let libraryButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        button.setBackgroundImage(UIImage(systemName: "photo"), for: .normal)
+        button.addTarget(self, action: #selector(getPhotoLibrary), for: .touchDown)
+        return button
+    }()
+    
+    let photoButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        button.setBackgroundImage(UIImage(systemName: "camera"), for: .normal)
+        button.addTarget(self, action: #selector(getPhotoCamera), for: .touchDown)
+        return button
+    }()
+    
+    let shareButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        button.setBackgroundImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        button.addTarget(self, action: #selector(sharePhoto), for: .touchDown)
+        return button
+    }()
+    
+    let rotateRigthButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        button.setBackgroundImage(UIImage(systemName: "rotate.right"), for: .normal)
+        button.addTarget(self, action: #selector(rotatedRigth), for: .touchDown)
+        return button
+    }()
+    
+    let rotateLeftButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        button.setBackgroundImage(UIImage(systemName: "rotate.left"), for: .normal)
+        button.addTarget(self, action: #selector(rotatedLeft), for: .touchDown)
+        return button
+    }()
+    
+    
+    private let widthPhoto: CGFloat = 180
+    private let heigthPhoto: CGFloat = 240
+    
+    private let widthButton: CGFloat = 40
+    private let heigthButton: CGFloat = 35
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        imagePhotoPicker.delegate = self
+        // добавили на наше view дополнительное subview и установили констрейнты для кнопок
+        setupImageScrollView(width: widthPhoto, heigth: heigthPhoto)
+        setupLibraryButton()
+        setupPhotoButton()
+        setupShareButton()
+        setupRotateLeftButton()
+        setupRotateRigthButton()
+        // выгружаем изображение в ScrollView
+        self.imageScroll.set(image: photoImage)
+    }
+    
+    // MARK: - 2objc - функции кнопок
+    @objc func getPhotoCamera() {
+        fetchImage(.camera)
+    }
+    
+    @objc func getPhotoLibrary() {
+        fetchImage(.gallary)
+    }
+    
+    @objc func sharePhoto() {
+        let shareImage = self.imageScroll.getImage(image: photoImage)
+        let shareController = UIActivityViewController(activityItems: [shareImage], applicationActivities: nil)
+        shareController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool { print("Yes")}
+        }
+        present(shareController, animated: true, completion: nil)
+    }
+    
+    @objc func rotatedRigth() {
+        let rotatedImage = photoImage.rotate(radians: .pi * 0.5)
+        photoImage = rotatedImage
+        self.imageScroll.set(image: photoImage)
+    }
+    
+    @objc func rotatedLeft() {
+        let rotatedImage = photoImage.rotate(radians: .pi * 1.5)
+        photoImage = rotatedImage
+        self.imageScroll.set(image: photoImage)
+    }
+    
+    //MARK: -  действия после окончания работы встроенных вью контроллеров
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // проверка какое фото используем, редактируемое или оригинальное
+        if let image = info[.editedImage] as? UIImage {
+            photoImage = image
+        } else {
+            guard  let image = info[.originalImage] as? UIImage else { return }
+            photoImage = image
+        }
+        // наложение фильтра
+        imageFilter(photoImage)
+        // выключение встроенного вью контроллера
+        imagePhotoPicker.dismiss(animated: true, completion: nil)
+        
+        self.imageScroll.set(image: photoImage)
+    }
+    
+    // MARK: -  настройка кнопок поворота
+    private func setupRotateRigthButton() {
+        view.addSubview(rotateRigthButton)
+        rotateRigthButton.translatesAutoresizingMaskIntoConstraints = false
+        // задали размеры
+        rotateRigthButton.widthAnchor.constraint(equalToConstant: widthButton).isActive = true
+        rotateRigthButton.heightAnchor.constraint(equalToConstant: heigthButton).isActive = true
+        // привязали к левому верхнему углу
+        rotateRigthButton.topAnchor.constraint(equalTo: imageScroll.bottomAnchor, constant: 5).isActive = true
+        rotateRigthButton.rightAnchor.constraint(equalTo: imageScroll.rightAnchor).isActive = true
+    }
+    
+    private func setupRotateLeftButton() {
+        view.addSubview(rotateLeftButton)
+        rotateLeftButton.translatesAutoresizingMaskIntoConstraints = false
+        // задали размеры
+        rotateLeftButton.widthAnchor.constraint(equalToConstant: widthButton).isActive = true
+        rotateLeftButton.heightAnchor.constraint(equalToConstant: heigthButton).isActive = true
+        // привязали к левому верхнему углу
+        rotateLeftButton.topAnchor.constraint(equalTo: imageScroll.bottomAnchor, constant: 5).isActive = true
+        rotateLeftButton.leftAnchor.constraint(equalTo: imageScroll.leftAnchor).isActive = true
+    }
+    
+    // MARK: -  настройка кнопки для сохранения фото
+    private func setupShareButton() {
+        view.addSubview(shareButton)
+        shareButton.translatesAutoresizingMaskIntoConstraints = false
+        // задали размеры
+        shareButton.widthAnchor.constraint(equalToConstant: widthButton).isActive = true
+        shareButton.heightAnchor.constraint(equalToConstant: heigthButton).isActive = true
+        // привязали к левому верхнему углу
+        shareButton.topAnchor.constraint(equalTo: imageScroll.bottomAnchor, constant: 5).isActive = true
+        shareButton.centerXAnchor.constraint(equalTo: imageScroll.centerXAnchor).isActive = true
+    }
+    
+    // MARK: -  настройка кнопки для доступа к камере
+    private func setupPhotoButton() {
+        view.addSubview(photoButton)
+        photoButton.translatesAutoresizingMaskIntoConstraints = false
+        // задали размеры
+        photoButton.widthAnchor.constraint(equalToConstant: widthButton).isActive = true
+        photoButton.heightAnchor.constraint(equalToConstant: heigthButton).isActive = true
+        // привязали к левому верхнему углу
+        photoButton.bottomAnchor.constraint(equalTo: imageScroll.topAnchor, constant: -5).isActive = true
+        photoButton.rightAnchor.constraint(equalTo: imageScroll.rightAnchor).isActive = true
+    }
+    
+    // MARK: -  настройка кнопки для доступа в библиотеку
+    private func setupLibraryButton() {
+        view.addSubview(libraryButton)
+        libraryButton.translatesAutoresizingMaskIntoConstraints = false
+        // задали размеры
+        libraryButton.widthAnchor.constraint(equalToConstant: widthButton).isActive = true
+        libraryButton.heightAnchor.constraint(equalToConstant: heigthButton).isActive = true
+        // привязали к левому верхнему углу
+        libraryButton.bottomAnchor.constraint(equalTo: imageScroll.topAnchor, constant: -5).isActive = true
+        libraryButton.leftAnchor.constraint(equalTo: imageScroll.leftAnchor).isActive = true
+    }
+    
+    // MARK: - устанавливаем констрейнты для ImageScrollView
+    private func setupImageScrollView(width:CGFloat, heigth: CGFloat) {
+        imageScroll.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        imageScroll = ImageScrollView(frame: CGRect(
+            x: (view.bounds.width - width) / 2,
+            y: (view.bounds.height - heigth) / 2,
+            width: width,
+            height: heigth))
+        view.addSubview(imageScroll)
+        // разрешили использовать констрейнты
+        imageScroll.translatesAutoresizingMaskIntoConstraints = false
+        // привязали к границам view
+        imageScroll.heightAnchor.constraint(equalToConstant: heigth).isActive = true
+        imageScroll.widthAnchor.constraint(equalToConstant: width).isActive = true
+        imageScroll.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageScroll.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+}
+
+extension MainViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    // MARK: - получение изображения в зависимости от типа
+    private func fetchImage(_ type: TypeSource) {
+        imagePhotoPicker.allowsEditing = false // разрешение редактирования встроенными методами
+        imagePhotoPicker.modalPresentationStyle = .pageSheet // вид представления встроенных инструментов
+        
+        switch type {
+        case .camera:
+            imagePhotoPicker.sourceType = .camera
+            imagePhotoPicker.cameraDevice = .front
+        case .gallary:
+            imagePhotoPicker.sourceType = .photoLibrary // указываем что используем
+        }
+        
+        present(imagePhotoPicker, animated: true, completion: nil) // запуск контроллера
+    }
+    
+    // MARK: - применение фильтра и вывод изображения во вью
+    private func imageFilter(_ image: UIImage) {
+        let orientation = image.imageOrientation
+        let scale = image.scale
+        let context = CIContext(options: nil)
+        let inputImage = CIImage(image: image)
+        
+        let currentFilter = CIFilter(name: "CIPhotoEffectMono")
+        currentFilter?.setDefaults()
+        currentFilter?.setValue(inputImage, forKey: kCIInputImageKey) // ключ определяет определяет входное изображение
+        //      currentFilter.setValue(0.9, forKey: kCIInputIntensityKey) - ключ определяет интенсивность фильта, для Монохрома не нужен
+        if let output = currentFilter?.outputImage {
+            if let cgImage = context.createCGImage(output, from: output.extent) {
+                let uiImage = UIImage(cgImage: cgImage, scale: scale, orientation: orientation)
+                photoImage = uiImage
+            }
+        }
+    }
+}
+
+// MARK: - расширение для UIImage с функцией поворота
+extension UIImage {
+    func rotate(radians: CGFloat) -> UIImage {
+        let rotatedSize = CGRect(origin: .zero, size: size)
+            .applying(CGAffineTransform(rotationAngle: CGFloat(radians)))
+            .integral.size
+        UIGraphicsBeginImageContext(rotatedSize)
+        if let context = UIGraphicsGetCurrentContext() {
+            let origin = CGPoint(x: rotatedSize.width / 2.0,
+                                 y: rotatedSize.height / 2.0)
+            context.translateBy(x: origin.x, y: origin.y)
+            context.rotate(by: radians)
+            draw(in: CGRect(x: -origin.y, y: -origin.x,
+                            width: size.width, height: size.height))
+            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return rotatedImage ?? self
+        }
+        return self
+    }
+}
